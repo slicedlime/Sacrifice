4e6d424b90445e07cf75f579d5bed2dcfb91b539
slicedlime <slicedlime@gmail.com>
2023-02-20 16:45:16 +0100

    Packaging script + more metadata

---
+++ package.py
@@ -0,0 +1,34 @@
+import os.path
+import json
+from zipfile import ZipFile
+
+def zip(zip_filename, folder, excludes = []):
+    with ZipFile(zip_filename, 'w') as zip:
+        # Iterate over all the files in directory
+        for folder_name, subfolders, filenames in os.walk(folder):
+            for filename in filenames:
+                # create complete filepath of file in directory
+                file_path = os.path.join(folder_name, filename)
+                file = os.path.basename(file_path)
+                if file in excludes or file[0] == '.':
+                    continue
+
+                relative = os.path.relpath(file_path, folder)
+                if relative[0] == '.':
+                    continue
+                # Add file to zip
+                print(f'{relative}')
+                zip.write(file_path, relative)
+
+with open('metadata.json', 'r') as file:
+    meta = json.load(file)
+zip_file = meta['package']
+source_folder = meta['source']
+excludes = meta.get('excludes', [])
+
+try:
+    os.remove(zip_file)
+except OSError:
+    pass
+
+zip(zip_file, source_folder, excludes)
